bda51e2df5cee881c41a73167d5b36e749d2fbb3
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2022-03-19 21:50:45 +0330

    Add init colors

--- app/src/main/java/me/hamsah/musiccompose/MainActivity.kt
+++ app/src/main/java/me/hamsah/musiccompose/MainActivity.kt
@@ -17,27 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MusicComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MusicComposeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/me/hamsah/musiccompose/ui/theme/Color.kt
+++ app/src/main/java/me/hamsah/musiccompose/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package me.hamsah.musiccompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkBackground = Color(0xFF13141E)
+val Silver = Color(0xFFF9F9FE)
+val LightPurple = Color(0xFF4E3D6F)
